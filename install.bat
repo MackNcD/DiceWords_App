954094cf0d14e01917df314665efff37c1d88644
ItsAleel <149256688+ItsAleel@users.noreply.github.com>
2023-10-28 12:46:08 -0400

    Update install.bat
    Updated the Install bat to install the .venv, activate the venv, install requirements, and create a Folder in Dicewords for templates named ' freshly created '

--- install.bat
+++ install.bat
@@ -8,23 +8,47 @@ if errorlevel 1 (
     exit
 )
 
+REM Store the original directory (location of the batch file)
+set original_dir=%~dp0
 
-REM Navigate to the main DiceWords folder
-cd /D %~dp0
-
+REM Navigate to the original directory (DiceWords_App-main)
+cd /D %original_dir%
 
 REM Create a python virtual environment
 python -m venv .venv
+if not exist ".venv" (
+    echo Failed to create the virtual environment.
+    pause
+    exit
+)
 
-
-REM Activate the virtual environment
-.venv\Scripts\activate
-
+REM Activate the virtual environment using the call command
+call .venv\Scripts\activate
 
 REM Install the dependencies
 pip install -r requirements.txt
 
-
 REM Inform the user to open main.py in Windows Studio Code
 echo Dependencies installed. Now, you can open main.py and run it in Windows Studio Code.
+pause
+
+REM Navigate to the dicewords directory inside DiceWords_App-main
+cd /D "%original_dir%dicewords"
+
+REM Check if the path exists before creating the folder
+if not exist "%original_dir%dicewords" (
+    echo The system cannot find the path specified.
+    pause
+    exit
+)
+
+REM Create the 'freshly created' folder
+mkdir "freshly created"
+
+REM Check if the folder was created successfully
+if exist "%original_dir%dicewords\freshly created" (
+    echo Folder 'freshly created' has been created.
+) else (
+    echo Failed to create the 'freshly created' folder.
+)
 pause
