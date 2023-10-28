899ec3c5613659b98b09a4ceb953065e234c89d3
Nicholas <42632395+MackNcD@users.noreply.github.com>
2023-10-28 11:42:03 -0400

    Update install.bat
    Thanks to Austin: Discord @Note

--- install.bat
+++ install.bat
@@ -1,3 +1,30 @@
-The soulmates had made megacities.
+@echo off
 
- Intricate Pillars, Suspended Bridges, Triangular Skylights, Symmetrical Rotundas, Hexagonal Chambers, Glass Tunnels, Angular Facades
+REM Check Python version
+python --version | findstr /R "3.10.*" > nul
+if errorlevel 1 (
+    echo Ensure you have Python 3.10 or higher installed.
+    pause
+    exit
+)
+
+
+REM Navigate to the main DiceWords folder
+cd /D %~dp0
+
+
+REM Create a python virtual environment
+python -m venv .venv
+
+
+REM Activate the virtual environment
+.venv\Scripts\activate
+
+
+REM Install the dependencies
+pip install -r requirements.txt
+
+
+REM Inform the user to open main.py in Windows Studio Code
+echo Dependencies installed. Now, you can open main.py and run it in Windows Studio Code.
+pause
